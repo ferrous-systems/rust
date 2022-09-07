855540b8ddfc54df17d946ae021210c8eec7bcac
Oli Scherer <git-spam-no-reply9815368754983@oli-obk.de>
2022-09-07 13:18:42 +0000

    Make regression test actually fail

--- src/test/ui/const-mut-refs-crate.rs
+++ src/test/ui/const-mut-refs-crate.rs
@@ -21,18 +21,5 @@ pub fn main() {
     assert_eq!(LOCAL_FOO as *const i32, LOCAL_BAR as *const i32);
     assert_eq!(*DOUBLE_REF as *const i32, ONE_STEP_ABOVE as *const i32);
 
-    // assert_eq!(FOO as *const i32, LOCAL_BAZ as *const i32);
-    // above fails
-    // ---- [ui] src/test/ui/const-mut-refs-crate.rs stdout ----
-    //
-    // error: test run failed!
-    // status: exit status: 101
-    // command: "..rust/build/x86_64-unknown-linux-gnu/test/ui/const-mut-refs-crate/a"
-    // stdout: none
-    // --- stderr -------------------------------
-    // thread 'main' panicked at 'assertion failed: `(left == right)`
-    //   left: `0x7f3f52fe5000`,
-    //  right: `0x55f4daa4b000`', ..rust/src/test/ui/const-mut-refs-crate.rs:20:5
-    // note: run with `RUST_BACKTRACE=1` environment variable to display a backtrace
-    // ------------------------------------------
+    assert_eq!(FOO as *const i32, LOCAL_BAZ as *const i32);
 }
